2a74bd225a30ac4597541fd515e61bf32a9d8fb6
Aleksey <aay92@yandex.ru>
2023-09-29 23:56:58 +0400

    view model

---
+++ TipCalculator/Model/ResultData.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  TipCalculator
+//
+//  Created by Aleksey Alyonin on 29.09.2023.
+//
+
+import UIKit
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill:       Double
+    let totalTill:       Double
+}
---
+++ TipCalculator/ViewModel/CalculaterVM.swift
@@ -0,0 +1,8 @@
+//
+//  CalculaterVM.swift
+//  TipCalculator
+//
+//  Created by Aleksey Alyonin on 29.09.2023.
+//
+
+import Foundation
