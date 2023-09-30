4665d669137c392dd38859241657189027429c49
Aleksey <aay92@yandex.ru>
2023-09-30 20:14:04 +0400

    Added new file vm

---
+++ TipCalculator/Extansion/Double + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Double + Extension.swift
+//  TipCalculator
+//
+//  Created by Aleksey Alyonin on 30.09.2023.
+//
+
+import Foundation
---
+++ TipCalculator/Extansion/String + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  String + Extension.swift
+//  TipCalculator
+//
+//  Created by Aleksey Alyonin on 30.09.2023.
+//
+
+import Foundation
