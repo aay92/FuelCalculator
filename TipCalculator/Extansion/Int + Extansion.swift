33639f66e1eaf15aaad8a0232dfb0a9c17faabbc
Aleksey <aay92@yandex.ru>
2023-10-02 20:14:50 +0400

    work and fix bug

---
+++ TipCalculator/Extansion/Int + Extansion.swift
@@ -0,0 +1,8 @@
+//
+//  Int + Extansion.swift
+//  TipCalculator
+//
+//  Created by Aleksey Alyonin on 02.10.2023.
+//
+
+import Foundation
---
+++ TipCalculator/Extansion/UIResponder + Extantion.swift
@@ -0,0 +1,8 @@
+//
+//  UIResponder + Extantion.swift
+//  TipCalculator
+//
+//  Created by Aleksey Alyonin on 01.10.2023.
+//
+
+import Foundation
